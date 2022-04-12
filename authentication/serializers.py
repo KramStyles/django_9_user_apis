d16f237df79700cd57f291dc6c6102819a2ee45e
kramstyles <mark.eke@decagon.dev>
2022-04-12 01:14:06 +0100

    authentication views.py

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,3 +9,6 @@ class RegisterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from authentication.serializers import RegisterSerializer
+
+
+class RegisterAPIView(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
