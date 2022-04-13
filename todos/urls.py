aae553b4cbd3bba3881d3e21b345e6f29ecfe381
kramstyles <mark.eke@decagon.dev>
2022-04-13 13:51:08 +0100

    included list_create api view

--- todos/urls.py
+++ todos/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('create/', views.CreateTodoAPIView.as_view(), name='create-todo'),
     path('list/', views.TodoListAPIView.as_view(), name='list-todo'),
+    path('list_create/', views.ListCreateTodoApi.as_view(), name='list-create-todo')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,20 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import TodoSerializer
 from .models import Todo
+from todos import serializers
+
+
+class ListCreateTodoApi(ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
 
 
 class CreateTodoAPIView(CreateAPIView):
@@ -12,10 +24,10 @@ class CreateTodoAPIView(CreateAPIView):
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
+
 class TodoListAPIView(ListAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated, )
-    
 
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
