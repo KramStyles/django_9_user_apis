3ea2055d74ded549ee071c7585a5f3d98d4dcf1f
kramstyles <mark.eke@decagon.dev>
2022-04-13 09:44:34 +0100

    worked on todos urls and view

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from helpers.models import TrackingModels
+from authentication.models import User
+
+class Todo(TrackingModels):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    is_finished = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from posixpath import isabs
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import TodoSerializer
+
+
+class CreateTodoAPIView(CreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
